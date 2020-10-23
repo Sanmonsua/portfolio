4e9008ce2eb166ff2ac7750f100451737301d994
Sanmonsua <sanmonsua@gmail.com>
2020-10-23 15:36:06 -0500

    Updates

---
+++ precache-manifest.6713efb80e2c7817fcb98fcde3b5ab24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d09923483d1badda0773b59637517e8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8783bfa979fa98b0c7b2",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "30b7bd8be8075dcda808",
+    "url": "/portfolio/static/js/2.c4c1a0cd.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/portfolio/static/js/2.c4c1a0cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8783bfa979fa98b0c7b2",
+    "url": "/portfolio/static/js/main.1fa6e603.chunk.js"
+  },
+  {
+    "revision": "6bafe6fdb4893a0fd8e5",
+    "url": "/portfolio/static/js/runtime-main.459797f6.js"
+  }
+]);
